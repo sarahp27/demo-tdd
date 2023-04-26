287fc00b3118441f89a4eca2d1991ba94fed7b8d
TahreemBakhtiar9 <tehreembakhtiar927@gmail.com>
2023-04-26 23:22:49 +0500

    date and edit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import TodoList from './components/TodoList';
-import './App.css';
-import { act } from '@testing-library/react';
-import TodoItem from './components/TodoItem';
+import React, { useState, useEffect } from "react";
+import TodoList from "./components/TodoList";
+import "./App.css";
+import { act } from "@testing-library/react";
+// import TodoItem from "./components/TodoItem";
+// import Card from "./components/Card";
 
 function App() {
-
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [newTodo, setNewTodo] = useState('');
+  const [newTodo, setNewTodo] = useState("");
   const [saving, setSaving] = useState(false);
+  const [date, setDate] = useState("");
+
   function onChange(e) {
     const value = e.target.value;
     setNewTodo(value);
   }
-  
+  function onChangeDate(e) {
+    console.log(e.target.value);
+    setDate(e.target.value);
+  }
+
   function removeTodo(id) {
-    setTodos(todos.filter(t => t.id !== id));
+    setTodos(todos.filter((t) => t.id !== id));
   }
 
   function updateTodo(id) {
-      setLoading(true);
+    setLoading(true);
     const newList = todos.map((todoItem) => {
       if (todoItem.id === id) {
         const updatedItem = { ...todoItem, completed: !todoItem.completed };
@@ -32,6 +38,21 @@ function App() {
     setLoading(false);
   }
 
+  function edit(id) {
+    let editedList = todos.map((e, i) => {
+      if (i == id - 1) {
+        // console.log("HEREEE", todos[id - 1]);
+        todos[id - 1] = {
+          userId: 1,
+          id: id,
+          title: prompt("new todo"),
+          completed: false,
+          date: prompt("date"),
+        };
+      }
+      setTodos([...todos]);
+    });
+  }
   function addTodo(e) {
     e.preventDefault();
     const value = {
@@ -39,14 +60,15 @@ function App() {
       id: Math.floor(Math.random() * 10000) + 1,
       title: newTodo,
       completed: false,
+      date: date,
     };
-  
+
     setSaving(true);
     fetch(process.env.REACT_APP_API_URL, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify(value),
       headers: {
-        'Content-type': 'application/json; charset=UTF-8',
+        "Content-type": "application/json; charset=UTF-8",
       },
     })
       .then((response) => response.json())
@@ -58,43 +80,43 @@ function App() {
 
   useEffect(() => {
     async function fetchData() {
-      const result = await fetch(process.env.REACT_APP_API_URL).then((response) =>
-        response.json()
+      const result = await fetch(process.env.REACT_APP_API_URL).then(
+        (response) => response.json()
       );
-      act (()=>{
+      act(() => {
         setTodos(result.slice(0, 5));
         setLoading(false);
-      })
-      
+      });
     }
     fetchData();
   }, []);
   return (
-  <div className="App">
-    <h1 className="header">My todo list</h1>
-    {loading ? (
-      'Loading'
-    ) : (
-      <TodoList todos={todos} removeHandler={removeTodo} updateTodo={updateTodo} />
-    )}
-
-    <div className="add-todo-form">
-      {saving ? (
-        'Saving'
+    <div className="App">
+      <h1 className="header">My todo list</h1>
+      {loading ? (
+        "Loading"
       ) : (
-        <form onSubmit={addTodo}>
-          <input type="text" onChange={onChange} />
-          <button type="submit">Add new todo</button>
-        </form>
+        <TodoList
+          todos={todos}
+          removeHandler={removeTodo}
+          updateTodo={updateTodo}
+          edit={edit}
+        />
       )}
-    </div>
-  </div>
-);
 
+      <div className="add-todo-form">
+        {saving ? (
+          "Saving"
+        ) : (
+          <form onSubmit={addTodo}>
+            <input type="text" onChange={onChange} />
+            <input type="date" onChange={onChangeDate} />
+            <button type="submit">Add new todo</button>
+          </form>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
-
-
-
-
